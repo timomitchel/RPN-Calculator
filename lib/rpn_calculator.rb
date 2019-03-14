350af8b2bbc4778d18155dacc02fa80afb9d452d
Timo Tyrrell <timomitchel23@gmail.com>
2019-03-14 11:44:48 -0600

    Add initial loop, operator constant, check and filter methods, and tests.

--- lib/rpn_calculator.rb
+++ lib/rpn_calculator.rb
@@ -1,8 +1,39 @@
 class RpnCalculator
 
+  OPERATORS = %w(+ - * /).freeze
+
   attr_reader :polish_expression, :stack
   def initialize
     @polish_expression = []
     @stack = []
+  end
+
+  def enter_loop
+    p 'Start entering the expression to evaluate in Reverse Polish Notation' if @polish_expression.length == 0
+    p "Continue entering the expression to evaluate in Reverse Polish Notation with #{@polish_expression.last} as the first number on the stack" if @polish_expression.length == 1
+    loop do
+      input = $stdin.gets.chomp
+      check_input(input)
+    end
+  end
+
+  def filter_input(input)
+    input.scan(/[-=\+\*\/0-9q]/).length == 0 || (input.length > 1 && !input.scan(/\D/).empty?)
+  end
+
+  def check_input(input)
+    if filter_input(input)
+        p 'Invalid character'
+        enter_loop
+    end
+    case input
+    when input == 'q'
+        p 'goodbye'
+        exit!
+    when input == '='
+      calculate_values(@polish_expression)
+    else
+      @polish_expression << input
+    end
   end
 end
--- test/Rpn_caluclator_test.rb
+++ test/Rpn_caluclator_test.rb
@@ -17,4 +17,34 @@ class RpnCalculatorTest < Minitest::Test
     assert @calculator.polish_expression.empty?
     assert @calculator.stack.empty?
   end
+
+  def test_operators_constant
+    assert_equal @calculator.singleton_class.constants[0], :OPERATORS
+  end
+
+  def test_check_input_with_valid_numbers
+    input = '13'
+    @calculator.check_input(input)
+    assert_equal @calculator.polish_expression.first, input
+    input = '5293094'
+    @calculator.check_input(input)
+    assert_equal @calculator.polish_expression.last, input
+    input = '5'
+    @calculator.check_input(input)
+    assert_equal @calculator.polish_expression.last, input
+  end
+
+  def test_filter_input
+    refute @calculator.filter_input(RpnCalculator::OPERATORS[0])
+    refute @calculator.filter_input(RpnCalculator::OPERATORS[1])
+    refute @calculator.filter_input(RpnCalculator::OPERATORS[2])
+    refute @calculator.filter_input(RpnCalculator::OPERATORS[3])
+    refute @calculator.filter_input('=')
+    assert @calculator.filter_input('')
+    assert @calculator.filter_input('w')
+    assert @calculator.filter_input('Z')
+    assert @calculator.filter_input('@')
+    assert @calculator.filter_input('5-')
+    assert @calculator.filter_input('+1')
+  end
 end
