ecd0fbdf209470cf5595bd5a33f8feb05eeead20
Timo Tyrrell <timomitchel23@gmail.com>
2019-03-17 22:02:23 -0600

    Update logic to make program more flexible with fractions and more numbers at the front of the expression

--- lib/rpn_calculator.rb
+++ lib/rpn_calculator.rb
@@ -54,35 +54,44 @@ class RpnCalculator
     restart_program
   end
 
+  def calculate_operator_input(operator)
+    numbers = @stack.pop(2)
+    binding.pry
+    if check_divide_by_zero(numbers, operator) || check_multiply_zero_by_another_number(numbers, operator)
+      @stack << 0
+    else
+      result = do_math_accordingly(operator, numbers)
+      @stack << result
+    end
+  end
+
+  def do_math_accordingly(operator, numbers)
+    return (numbers.last.to_f + numbers.first.to_f) if operator == OPERATORS[0]
+    return (numbers.last.to_f - numbers.first.to_f) if operator == OPERATORS[1] && @polish_expression.last != '-'
+    return (numbers.last.to_f * numbers.first.to_f) if operator == OPERATORS[2]
+    return (numbers.last.to_f / numbers.first.to_f) if operator == OPERATORS[3]
+    return (numbers.first.to_f - numbers.last.to_f) if operator == OPERATORS[1]
+  end
+
+  def check_divide_by_zero(numbers, operator)
+    numbers.last == 0 && operator == RpnCalculator::OPERATORS.last
+  end
+
+  def check_multiply_zero_by_another_number(numbers, operator)
+    numbers.first == 0 && operator == RpnCalculator::OPERATORS[2]
+  end
+
   def restart_program
-    p @stack.last
+    if @stack.last.to_i
+      p @stack.last.to_i
+    else
+      p @stack.last
+    end
     @polish_expression.clear
-    @polish_expression << @stack.last.to_s
+    @stack.each do |element|
+      @polish_expression << element.to_s
+    end
     @stack.clear
     run
-  end
-
-  def filter_input(input)
-    input.scan(/[-=\+\*\/0-9q]/).length == 0 || (input.length > 1 && !input.scan(/\D/).empty?)
-  end
-
-  def check_filter_input(input)
-    if filter_input(input)
-        p 'Invalid character'
-        run
-    end
-  end
-
-  def check_input(input)
-    check_filter_input(input)
-    case input
-    when RpnCalculator::SPECIAL_INPUT_CHARACTERS.first
-        p 'goodbye'
-        exit!
-    when RpnCalculator::SPECIAL_INPUT_CHARACTERS.last
-      calculate_values(@polish_expression)
-    else
-      @polish_expression << input
-    end
   end
 end
