ce87d6180d8e64e7bc698669e9b7b1b7aef52052
Timo Tyrrell <timomitchel23@gmail.com>
2019-03-17 22:00:58 -0600

    Update messaging and method order for ease of talking through code

--- lib/rpn_calculator.rb
+++ lib/rpn_calculator.rb
@@ -11,29 +11,36 @@ class RpnCalculator
 
   def run
     p 'Start entering the expression to evaluate in Reverse Polish Notation' if @polish_expression.length == 0
-    p "Continue entering the expression to evaluate in Reverse Polish Notation with #{@polish_expression.last} as the first number on the stack" if @polish_expression.length == 1
+    p "Continue entering the expression to evaluate in Reverse Polish Notation with #{@polish_expression.last} as the only number/decimal on the stack" if @polish_expression.length == 1
+    p "Continue entering the expression to evaluate in Reverse Polish Notation with #{@polish_expression} as the numbers still waiting to be evaluated" if @polish_expression.length > 1
     loop do
       input = $stdin.gets.chomp
       check_input(input)
     end
   end
 
-  def check_divide_by_zero(numbers, operator)
-    numbers.last == 0 && operator == RpnCalculator::OPERATORS.last
-  end
-
-  def check_multiply_zero_by_another_number(numbers, operator)
-    numbers.first == 0 && operator == RpnCalculator::OPERATORS[2]
-  end
-
-  def calculate_operator_input(operator)
-    numbers = @stack.pop(2)
-    if check_divide_by_zero(numbers, operator) || check_multiply_zero_by_another_number(numbers, operator)
-      @stack << 0
+  def check_input(input)
+    check_filter_input(input)
+    case input
+    when RpnCalculator::SPECIAL_INPUT_CHARACTERS.first
+        p 'goodbye'
+        exit!
+    when RpnCalculator::SPECIAL_INPUT_CHARACTERS.last
+      calculate_values(@polish_expression)
     else
-      result = numbers.reduce(operator)
-      @stack << result
+      @polish_expression << input
     end
+  end
+
+  def check_filter_input(input)
+    if filter_input(input)
+        p 'Invalid character'
+        run
+    end
+  end
+
+  def filter_input(input)
+    input.scan(/[-=\+\*\/0-9q]/).length == 0 || (input.length > 1 && !input.scan(/\D/).empty?)
   end
 
   def calculate_values(expression)
